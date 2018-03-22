dbdd033f30d08ddfb35bd1e4814cdd17e9f462d5
breakawayz <375831500@qq.com>
2018-03-22 09:47:24 +0800

    新增不同内容批量发送通知类短信

--- smn-sdk-net-example/SmsDemo.cs
+++ smn-sdk-net-example/SmsDemo.cs
@@ -98,14 +98,85 @@ namespace Smn.Example
             }
         }
 
+        public void SmsBatchPublishWithDiffMessage()
+        {
+            SmsPublishMessage smsPublishMessage1 = new SmsPublishMessage()
+            {
+                MessageIncludeSignFlag = false,
+                // 短信签名,需要在消息通知服务的自助页面申请签名，申请办理时间约2天
+                SignId = "3fe9fae14729495990cf1a3218fe2aca",
+                Endpoint = "1368****87",
+                Message = "你好，您的验证码是:12345679，请查收"
+            };
+
+            SmsPublishMessage smsPublishMessage2 = new SmsPublishMessage()
+            {
+                // MessageIncludeSignFlag默认为false，SignId必填，
+                // 为true时，可以不传SignId, 但是内容中必须包含签名,如【华为云】您的验证码是:1234，请查收。签名以【】括起来放在内容头部或者尾部
+                MessageIncludeSignFlag = true,
+                // 短信签名,需要在消息通知服务的自助页面申请签名，申请办理时间约2天
+                Endpoint = "186****875",
+                Message = "【华为云华南测试】你好，您的验证码是:1234567，请查收"
+            };
+
+            SmsPublishMessage smsPublishMessage3 = new SmsPublishMessage()
+            {
+                MessageIncludeSignFlag = false,
+                // 短信签名,需要在消息通知服务的自助页面申请签名，申请办理时间约2天
+                SignId = "6be340e91e5241e4b5d85837e6709104",
+                Endpoint = "12345a bdd",
+                Message = "你好，您的验证码是:1234567，请查收"
+            };
+
+            List<SmsPublishMessage> smsMessages = new List<SmsPublishMessage>
+            {
+                smsPublishMessage1,
+                smsPublishMessage2,
+                smsPublishMessage3
+            };
+            SmsBatchPublishWithDiffMessageRequest request = new SmsBatchPublishWithDiffMessageRequest()
+            {
+                SmsMessages = smsMessages
+            };
+
+            try
+            {
+                // 发送请求并返回响应
+                SmsBatchPublishWithDiffMessageResponse response = smnClient.SendRequest(request);
+                if (response.IsSuccess())
+                {
+                    List<SmsBatchPublishWithDiffMessageResult> results = response.Result;
+                    foreach (SmsBatchPublishWithDiffMessageResult result in results)
+                    {
+                        Console.WriteLine("messageId={0}, endpoint={1}, code={2}, message={3}",
+                            result.MessageId, result.Endpoint, result.Code, result.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("code={0}, message={1}, statusCode={2}",
+                        response.ErrCode, response.ErrMessage, response.StatusCode);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                // 处理异常
+                Console.WriteLine("{0}", e.Message);
+                Console.ReadLine();
+            }
+        }
+
         /// <summary>
         /// 发送短信推广类
         /// </summary>
         public void PromotionSmsPublish()
         {
             // 发送手机号码 号码格式 (+)(国家码)(手机号码)
-            List<string> endpoints = new List<string>();
-            endpoints.Add("8613688807587");
+            List<string> endpoints = new List<string>
+            {
+                "8613688807587"
+            };
             // 设置请求对象
             PromotionSmsPublishRequest request = new PromotionSmsPublishRequest
             {
---
+++ smn-sdk-net/request/sms/SmsBatchPublishWithDiffMessageRequest.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * Copyright (C) 2017. Huawei Technologies Co., LTD. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of Apache License, Version 2.0.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Apache License, Version 2.0 for more details.
+ */
+using Smn.Http;
+using System;
+using System.Text;
+using Smn.Util;
+using Smn.Response.Sms;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Smn.Request.Sms
+{
+    ///<summary> 
+    /// smn batch publish with diff message request
+    /// author:zhangyx
+    /// version:1.0.3
+    ///</summary> 
+    public class SmsBatchPublishWithDiffMessageRequest : AbstractRequest<SmsBatchPublishWithDiffMessageResponse>
+    {
+        /// <summary>
+        /// sms_message
+        /// </summary>
+        private List<SmsPublishMessage> smsMessages;
+
+        [JsonProperty("sms_message")]
+        public List<SmsPublishMessage> SmsMessages { get => smsMessages; set => smsMessages = value; }
+
+        public override HttpMethod GetHttpMethod()
+        {
+            return HttpMethod.POST;
+        }
+
+        public override string GetUrl()
+        {
+            if (smsMessages == null || smsMessages.Count == 0)
+            {
+                throw new ArgumentException("smsMessages is empty");
+            }
+
+            if(smsMessages.Count > ValidateUtil.MAX_SMS_BATCH_PUBLISH_SIZE)
+            {
+                throw new ArgumentException("smsMessages size must be less than 1000");
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(GetSmnServiceUrl());
+            sb.Append(Constants.URL_DELIMITER).Append(Constants.V2).Append(Constants.URL_DELIMITER)
+                    .Append(ProjectId).Append(Constants.URL_DELIMITER).Append(Constants.SMN_NOTIFICATIONS)
+                    .Append(Constants.URL_DELIMITER).Append(Constants.SMN_SUB_PROTOCOL_BATCH_SMS);
+            return sb.ToString();
+        }
+    }
+
+    /// <summary>
+    /// the message data for SmsBatchPublishWithDiffMessageRequest
+    /// </summary>
+    public class SmsPublishMessage
+    {
+        /// <summary>
+        /// message access point
+        /// </summary>
+        private String endpoint;
+
+        /// <summary>
+        /// message to send
+        /// </summary>
+        private String message;
+
+        /// <summary>
+        /// message signature id
+        /// </summary>
+        private String signId;
+
+        /// <summary>
+        /// message content contain sign flag
+        /// </summary>
+        private bool messageIncludeSignFlag = false;
+
+        [JsonProperty("endpoint")]
+        public string Endpoint { get => endpoint; set => endpoint = value; }
+        [JsonProperty("message")]
+        public string Message { get => message; set => message = value; }
+        [JsonProperty("sign_id")]
+        public string SignId { get => signId; set => signId = value; }
+        [JsonProperty("message_include_sign_flag")]
+        public bool MessageIncludeSignFlag { get => messageIncludeSignFlag; set => messageIncludeSignFlag = value; }
+    }
+}
